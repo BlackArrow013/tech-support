df6b332967bbd5de712b15a64c55b19f037dc4f9
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2018-02-07 20:48:18 +0100

    Se han añadido respuestas adicionales al programa.

--- Responder.java
+++ Responder.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+import java.util.ArrayList;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -7,11 +9,20 @@
  */
 public class Responder
 {
+    private Random random;
+    private ArrayList<String> coleccion;
     /**
      * Construct a Responder - nothing to do
      */
     public Responder()
     {
+        random = new Random();
+        coleccion = new ArrayList<String>();
+        coleccion.add("Hola");
+        coleccion.add("Estamos trabajando en ello");
+        coleccion.add("¿En qué puedo ayudarte?");
+        coleccion.add("Esperemos que se solucione pronto tu problema");
+        coleccion.add("Un placer conocerte. Cuéntame.");        
     }
 
     /**
@@ -20,6 +31,6 @@ public class Responder
      */
     public String generateResponse()
     {
-        return "That sounds interesting. Tell me more...";
+        return coleccion.get(random.nextInt(coleccion.size()));
     }
 }
